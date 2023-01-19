fb21652a5efce539b6b7d972abc282be5b2020bf
Aaron <aaronlivias0412@gmail.com>
2023-01-19 08:09:47 -0500

    Se le actualizo, dandole mayor dinamismo a la pagina web.

---
+++ memoria.js
@@ -0,0 +1,211 @@
+//document.write("hola") escribe hola en el html
+console.log("Esto se va a visualizar en consola en inspeccionar elemento")
+let board;//definir una variable indefinido, board=nombre de una variable
+//Las siguientes 2 variables sirven para que al momento de encontrar dos emoji iguales se mantengan
+//VISIBLES
+let estadoJuego = 1
+let casillaTemporal = null
+//Creamos este arreglo, elegimos aleatoriamente uno de sus elemento y lo ponemos en la matrix en orden...
+//luego ese emoji elegido lo quitamos de este array... haciendose mas pequeño
+let simbolosDisponible = ["🐻", "🐷", "🐯", "🐰", "🐱", "🐲", "🐳", "🐵"]
+
+/*
+[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+*/
+//Crear una matrix
+function createBoard(numFilas, numColumnas){
+    const rows = [] //¿Por que "const" si sabemos que se usa para mantener un dato de manera fija?
+    //rows apunta o referencia un espacio vacio
+    //Ese espacio que esta apuntando, lo estoy agregando valores de manera indefinida
+    //La direccion de memoria se mantiene constante y por eso es tipo "const"
+
+    //Si quieres cambiar su direccion poner...
+    //rows=[2,3]
+
+    //Crear arreglo temporal de simbolos, con 2 emoji del mismo tipo
+    const numElementosArregloTemporal = (numFilas*numColumnas)/2
+    const arrSimbolosTemporal = []
+    for(let i=0; i<numElementosArregloTemporal; i++){
+        arrSimbolosTemporal.push(simbolosDisponible[i])
+        arrSimbolosTemporal.push(simbolosDisponible[i])
+    }
+
+    //La funcion random sale decimal. Redondear hacia arriba con un floor 
+    //Math.random()*5 = el numero va de 0 a 4
+    //Math.floor(Math.random())
+
+    for (let i = 0; i < numFilas;  i++) {
+        const casilla=[]
+        for(let j = 0; j < numColumnas; j++){
+            const posicionAlAzar = Math.floor(
+                Math.random()*arrSimbolosTemporal.length//longitud de la matrix Temporal
+            )
+            const simboloElegido = arrSimbolosTemporal[posicionAlAzar]
+            //array.splice(0,2) → eliminar 2 elementos desde la posicion 0 del array.
+            arrSimbolosTemporal.splice(posicionAlAzar,1)
+            const dataCasilla={
+                simbolo : simboloElegido, //Aca deberia ir simbolo:simboloElegido "🐵"
+                /*
+                copiar y pegar emoji https://www.w3schools.com/charsets/ref_emoji.asp 
+                */
+                visible : false
+            }
+            casilla.push(dataCasilla)//push: agrega el elemnto 0 o un mono
+        }
+        rows.push(casilla)//push: agrega el elemnto casilla
+    }
+    return rows
+}
+
+//Imprimir matrix
+function printBoard(board){
+    //Con este for yo puedo iterar con todos los elementos del arreglo, ya no necesito poner un length.array
+    let filaStr=""
+    for(let fila of board){
+        for(let casilla of fila){
+            if(casilla.visible === true){
+                filaStr = filaStr + "\t" +casilla.simbolo + ""
+            }else{
+                filaStr = filaStr + "\t" + "⬜" + ""
+            }
+            
+        }
+        filaStr +="\n \n"
+    }
+    console.log(filaStr)
+}
+
+// mosntrar la casilla en la posicion 2, 2. recuerda q empieza de 0.
+function mostrarCasilla(board, fila, columna){
+    const dataCasilla = board[fila][columna]
+    dataCasilla.visible = true
+}
+
+// ocultar la casilla en la posicion 2, 2. recuerda q empieza de 0.
+function ocultarCasilla(board, fila, columna){
+    const dataCasilla = board[fila][columna]
+    dataCasilla.visible = false
+}
+
+//Que consiga
+//usamos data casilla, para no usar las posiciones, x1, y1 x2, y3: es demaciado texto.
+function verificarMAtchCasillas(dataCasilla1, dataCasilla2){
+    return dataCasilla1.simbolo === dataCasilla2.simbolo
+}
+
+function crearDivFila(){
+    const divFila = document.createElement("div")
+    divFila.setAttribute("class", "row")
+
+    return divFila
+}
+
+function ocultra(casilla1, casilla2){
+    casilla1.visible = false
+    casilla2.visible = false
+}
+
+//evt esta el elmento  en donde se ah echo el click
+function buttOnClik(evt){
+    const butClickeado = evt.target
+    const fila = butClickeado.getAttribute("fila")
+    const columna = butClickeado.getAttribute("Columna")
+
+    const casilla = board[fila][columna]
+    //Cambiar el visible de false a true. Con "!" significa la negacion del valor anterior
+    //Si es true sera false y si es false sera true
+    casilla.visible = !casilla.visible
+    //console.log("Se hizo click en un boton") imprimir
+    //console.log(evt.target) tener referencia del boton
+    if(estadoJuego==1){
+        casillaTemporal = casilla
+        estadoJuego = 2
+    }else{
+        estadoJuego = 1
+        //Verificar si hubo match, para luego pasarlo al estado 1.
+        const resultado = verificarMAtchCasillas(casillaTemporal, casilla)
+        //Si es resultado es verdadero, significa que son las mismas... hubo macth
+        if(resultado==false){
+            casillaTemporal.visible = false
+            casilla.visible = false
+        }
+    }
+    renderizarBoard(board)
+}
+
+function crearDivColumna(fil, col, simbolo, visible){
+    const divColumna = document.createElement("div")
+    divColumna.setAttribute("class", "col")
+
+    //Crear el boton hijo
+    const divBut = document.createElement("button")
+    divBut.setAttribute("type", "button")
+    divBut.setAttribute("class", "btn btn-success")
+    divBut.setAttribute("style","font.size:40px;")
+    divBut.setAttribute("fila", fil)
+    divBut.setAttribute("columna", col)
+
+    //Pone invisible los emoji
+    if(visible == true){
+        divBut.innerText = simbolo
+    }else {
+        divBut.innerText = ""
+    }
+    divBut.onclick = buttOnClik;// No estoy ejecutando la funcion "buttOnClik()"-->NO
+    //Pasarle la funcion a un objeto, la funcion se ejecutara cuando se haga "clik"
+
+    //Agregar el boton al divColumna
+    divColumna.appendChild(divBut)
+
+    return divColumna
+}
+
+function renderizarBoard(board){
+    //el divBoard es la dicc del arbol, que es la "id" del unico "div" del html
+    //Para que al ejecutar .innerHTML="" borrara solo lo que este dentro del div
+    //Necesitamos borrar porque cada vez que hacemos click estamos creando una nueva
+    //tabla. Por lo que necesitamos borrar la antigua. Sino veremos 2 tablas o matrices
+    const divBoard = document.getElementById("board")
+    divBoard.innerHTML=""
+    //Hasta aqui es de borrar
+
+    //document.body.innerHTML=""
+
+    //Aca crear la matriz o tabla
+    for(let i = 0; i<board.length; i++){
+        const fila = board[i]
+        let divFila = crearDivFila()
+        for(let j = 0; j < fila.length; j++){
+            const casilla = fila[j]
+            let divColumna = crearDivColumna(i, j, casilla.simbolo, casilla.visible)
+            //Agregamos a la fila una columna
+            divFila.appendChild(divColumna)
+        }
+        divBoard.appendChild(divFila)
+    }
+}
+
+function main(){
+    board = createBoard(3,3)
+    printBoard(board)
+    renderizarBoard(board)
+    /*
+    console.log(
+        verificarMAtchCasillas(
+        board[2][2],
+        board[1][1]
+        )
+    )
+    
+    Conclusion
+    Deberia retornarte False porque es Ya tenemos una matrix con diferentes emojis, 
+    cada casilla es un "objeto" que tiene 2 atributos (simbolo, visible), 
+    el verificarMAtchCasilla verifica el "simbolo", pero lo que se imprime es lo visible.
+    */
+}
+
+main()
